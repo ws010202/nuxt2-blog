fe3f15606e1fa7c4f7e999ca2d10e72f3ca6d681
ws010202 <102277644+ws010202@users.noreply.github.com>
2022-03-24 13:22:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
   title: 'yun yu yuan',
-  githubName: 'yunyuyuan',
+  githubName: 'ws010202',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'master',
   domain: 'https://blog.halberd.cn',
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: 'ws010202博客，书写值得书写的东西',
+  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,ws010202博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
 }
